624c5606bf2c6e91e2f62838b54ff82ca6b06509
Joaquín <joarodmar2@us.es>
2023-03-03 16:59:30 +0100

    L05

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 var port = process.env.PORT || 12345;
 
 app.get("/faces", (request,response) => {
-    res.send(cool());
+    response.send(cool());
     console.log("New request");
 });
 
@@ -41,4 +41,39 @@ app.get("/samples/RDQ", (request,response) => {
     console.log("New request");
 });
 
-
+app.get("/samples/RPP", (request,response) =>{
+    var raw_data = [ 
+        {year:2016, city:"Almería", protected_space:18, area: 163.937, fire: 57},
+        {year:2016, city:"Cádiz", protected_space:29, area: 231.22, fire: 108},
+        {year:2016, city:"Córdoba", protected_space:19, area: 134.597, fire: 90},
+        {year:2016, city:"Granada", protected_space:17, area: 220.314, fire: 119},
+        {year:2016, city:"Huelva", protected_space:24, area: 319.11, fire: 155},
+        {year:2016, city:"Jaén", protected_space:17, area: 317.381, fire: 172},
+        {year:2016, city:"Málaga", protected_space:28, area: 89.272, fire: 111},
+        {year:2016, city:"Sevilla", protected_space:24, area: 220.868, fire: 124},
+        {year:2017, city:"Almería", protected_space:18, area: 163.937, fire: 87},
+        {year:2017, city:"Cádiz", protected_space:29, area: 231.22, fire: 94},
+        {year:2017, city:"Córdoba", protected_space:19, area: 134.597, fire: 78},
+        {year:2017, city:"Almería", protected_space:29, area: 123.937, fire: 95},
+        
+      ];
+      
+      avg  = 0;
+      sum = 0;
+      avg1=0;
+      sum2=0;
+      var data = raw_data.filter(cities => cities.city == "Almería" );
+      var data2 = raw_data.filter(cities => cities.city == "Córdoba" );
+      
+      for (let i =0;i<data.length;i++){
+          var sum = sum + data[i].fire;
+          var avg =  sum/data.length;
+      }
+      for (let i =0;i<data2.length;i++){
+          var sum2 = sum2 + data2[i].fire;
+          var avg1 =  sum2/data2.length;
+      }
+      
+     console.log("Fire avg in Almería is ", avg);
+      response.send(`Fire average in Almería is: ${avg}`)
+});
