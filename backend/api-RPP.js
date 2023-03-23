6f4ef7bb4315a9f3ede4ed7e704daa8a3d85b2c4
ruspat1 <ruspat1@us.es>
2023-03-23 12:54:53 +0100

    23M

--- backend/api-RPP.js
+++ backend/api-RPP.js
@@ -21,45 +21,137 @@ module.exports = (app) => {
         { year: 2017, city: "Almería", protected_space: 29, area: 123.937, fire: 95 },
 
     ];
+    //tildes
+    function stripAccents(text) {
+        return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    }
+  //  db.insert(environment_stats);
 
 
-    app.get(BASE_API_URL + "/environment-stats/loadInitialData", (request, response) => {
-        if (environment_stats.length === 0) {
-            environment_stats.push(
-                { year: 2016, city: "Almería", protected_space: 18, area: 163.937, fire: 57 },
-                { year: 2016, city: "Cádiz", protected_space: 29, area: 231.22, fire: 108 },
-                { year: 2016, city: "Córdoba", protected_space: 19, area: 134.597, fire: 90 },
-                { year: 2016, city: "Granada", protected_space: 17, area: 220.314, fire: 119 },
-                { year: 2016, city: "Huelva", protected_space: 24, area: 319.11, fire: 155 },
-                { year: 2016, city: "Jaén", protected_space: 17, area: 317.381, fire: 172 },
-                { year: 2016, city: "Málaga", protected_space: 28, area: 89.272, fire: 111 },
-                { year: 2016, city: "Sevilla", protected_space: 24, area: 220.868, fire: 124 },
-                { year: 2017, city: "Almería", protected_space: 18, area: 163.937, fire: 87 },
-                { year: 2017, city: "Cádiz", protected_space: 29, area: 231.22, fire: 94 },
-            );
-            console.log("Inserted data into environment_stats");
-            db.insert(environment_stats, (err, docs) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(`Inserted ${docs.length} documents into environment_stats`);
-                }
-            });
-        }
-        response.json(environment_stats);
-        console.log("New GET to /environment-stats");
-    });
-    
-    app.get(BASE_API_URL + "/environment-stats", (request, response) => {
-        db.find({}, (err, environment_stats) => {
-            if (err) {
-                console.error(err);
-                response.status(500).send(err);
+    app.get(BASE_API_URL+"/environment-stats/loadInitialData", (req,res) => {
+       
+        db.find({},function(err, docs){
+
+           if(err){
+               res.sendStatus(500, "ERROR IN CLIENT");
+               return;
+           }
+           else{
+               db.remove({}, { multi: true }, (err,num)=>{
+                   if (err){
+                       res.sendStatus(500,"ERROR IN CLIENT");
+                       return;
+                   }
+                   return;
+               });
+       var iniData = [ { year: 2016, city: "Almería", protected_space: 18, area: 163.937, fire: 57 },
+       { year: 2016, city: "Cádiz", protected_space: 29, area: 231.22, fire: 108 },
+       { year: 2016, city: "Córdoba", protected_space: 19, area: 134.597, fire: 90 },
+       { year: 2016, city: "Granada", protected_space: 17, area: 220.314, fire: 119 },
+       { year: 2016, city: "Huelva", protected_space: 24, area: 319.11, fire: 155 },
+       { year: 2016, city: "Jaén", protected_space: 17, area: 317.381, fire: 172 },
+       { year: 2016, city: "Málaga", protected_space: 28, area: 89.272, fire: 111 },
+       { year: 2016, city: "Sevilla", protected_space: 24, area: 220.868, fire: 124 },
+       { year: 2017, city: "Almería", protected_space: 18, area: 163.937, fire: 87 },
+       { year: 2017, city: "Cádiz", protected_space: 29, area: 231.22, fire: 94 },
+       { year: 2017, city: "Córdoba", protected_space: 19, area: 134.597, fire: 78 },
+       { year: 2017, city: "Almería", protected_space: 29, area: 123.937, fire: 95 },
+        
+       ];
+       iniData.forEach( (e) => {
+           db.insert(e);
+       });
+       res.sendStatus(200,"OK");
+   }
+   });
+  });
+
+ 
+app.get(BASE_API_URL + "/environment-stats", (req, res) => {
+    db.find({}, { _id: 0 }, function(err, docs) {
+        if (err) {
+            res.status(500).send("ERROR IN CLIENT");
+        } else {
+            if (docs.length === 0) {
+                res.json([]);
             } else {
-                response.json(environment_stats);
+                res.json(docs);
             }
-        });
+        }
     });
+});
+
+
+
+app.get(BASE_API_URL + "/environment-stats/:city", (req, res) => {
+    var iuvcity = req.params.city;
+    //búsqueda
+    db.find({city: iuvcity}, function(err,docs){
+        if(err){
+            res.sendStatus(500, "ERROR IN CLIENT");
+            return;
+        }
+        else{
+            if(docs == 0){
+                res.sendStatus(404, "NOT FOUND")
+            }
+            else{
+                docsCopy = docs;
+                docsCopy.forEach((e) => {
+                    delete e._id;
+                })
+                res.send(JSON.stringify(docsCopy,null,2));
+            }
+        }
+    });
+});
+
+
+
+
+app.get(BASE_API_URL + "/environment-stats/:city/:year", (req, res) => {
+    var iuvcity = req.params.city;
+    var iuvYear = req.params.year;
+    db.find({city: iuvcity, year: parseInt(iuvYear)}, function(err,docs){
+        if(err){
+            res.sendStatus(500,  "ERROR IN CLIENT");
+            return;
+        }
+        else{
+            if(docs == 0){
+                res.sendStatus(404, "NOT FOUND")
+            }
+            else{
+                docsCopy = docs;
+                docsCopy.forEach((e) => {
+                    delete e._id;
+                })
+                res.send(JSON.stringify(docsCopy[0],null,2));
+            }
+        }
+    });
+});
+
+
+
+
+
+
+    // FUNCION COMPROBAR QUERYS
+    function compruebaQuery(e) {
+        if(e.length == 0){
+            return true;
+        }
+        else{
+            for (var i = 0; i < e.length; i++) {
+                var query = e[i];
+                if (query != "year" && query != "from" && query != "to" && query != "limit" && query != "offset") {
+                    return false;
+                }
+            }
+        }
+    }
+
 
     app.post(BASE_API_URL + "/environment-stats", (req, res) => {
         const newStat = req.body;
@@ -68,102 +160,45 @@ module.exports = (app) => {
             res.sendStatus(400);
             return;
         }
-        
-        const statIndex = environment_stats.findIndex(
-            (stat) =>
-                stat.year === newStat.year &&
-                stat.city.toLowerCase() === newStat.city.toLowerCase() &&
-                stat.protected_space === newStat.protected_space &&
-                stat.area === newStat.area &&
-                stat.fire === newStat.fire
-        );
     
-        if (statIndex !== -1) {
-            // If stat exists Conflict 409
-            console.log(`Conflict: environment stat with same properties already exists`);
-            res.sendStatus(409);
-            return;
-        }
-    
-        // If stat doesn´t exist Status 201
-        environment_stats.push(newStat);
-        console.log("Environment stat added to array");
-    
-        // Insert newStat in the database
-        db.insert(newStat, (err, newDoc) => {
+        db.findOne({
+            year: newStat.year,
+            city: newStat.city,
+            protected_space: newStat.protected_space,
+            area: newStat.area,
+            fire: newStat.fire
+        }, (err, stat) => {
             if (err) {
-                console.log(`Error inserting newStat into database: ${err}`);
+                console.log(`Error finding environment stat: ${err}`);
                 res.sendStatus(500);
-            } else {
-                console.log(`newStat with id ${newDoc._id} inserted into database`);
-                res.status(201).json(newDoc);
+                return;
             }
-        });
     
-        console.log(`new_stat = <${JSON.stringify(newStat, null, 2)}>`);
-        console.log("New POST to /environment-stats");
-    });
-    
-
-    app.delete(BASE_API_URL + "/environment-stats", (req, res) => {
-        db.remove({}, { multi: true }, (err, numRemoved) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send(err);
-            } else {
-                console.log(`All ${numRemoved} environment stats from database`);
-                res.sendStatus(204);
-                environment_stats=[];
+            if (stat !== null) {
+                // If stat exists Conflict 409
+                console.log(`Conflict: environment stat with same properties already exists`);
+                res.sendStatus(409);
+                return;
             }
-        });
-    });
-    //tildes
-    function stripAccents(text) {
-        return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    }
-
-    app.get(BASE_API_URL+"/environment-stats/:province", (request,response) => {
-        var provincia = request.params.province;
     
-        console.log(`New GET to /environment-stats/${provincia}`);
-    
-        db.find({province : provincia}).skip(0).limit(environment_stats.length).exec((err, data) =>{
-            if(err){
-                console.log(`Error geting /environment-stats/${provincia}: ${err}`);
-                response.sendStatus(500);
-            }else{
-                if(data.length!= 0){
-                    console.log(`data returned ${data.length}`);
-                    response.json(data.map((d)=>{
-                        delete d._id;
-                        return d;
-                    }));
+            // If stat doesn´t exist Status 201
+            db.insert(newStat, (err, newDoc) => {
+                if (err) {
+                    console.log(`Error inserting newStat into database: ${err}`);
+                    res.sendStatus(500);
+                } else {
+                    console.log(`newStat with id ${newDoc._id} inserted into database`);
+                    const responseObj = { ...newDoc };
+                    delete responseObj._id;
+                    res.status(201).json(responseObj);
                 }
-                 else{
-                    console.log(`Data not found /environment-stats/${provincia}: ${err}`);
-                    response.status(404).send("Data not found");
-                 }        
-            }
+            });
+    
+            console.log(`new_stat = <${JSON.stringify(newStat, null, 2)}>`);
+            console.log("New POST to /environment-stats");
         });
     });
     
-  
-
-    app.get(BASE_API_URL + "/environment-stats/:city/:year", (request, response) => {
-
-        const city = stripAccents(request.params.city.toLowerCase());
-        const year = request.params.year;
-
-        const cityStats = environment_stats.filter(stat => stripAccents(stat.city.toLowerCase()) === city && stat.year === parseInt(year));
-        if (cityStats.length > 0) {
-            response.json(cityStats);
-        } else {
-            response.sendStatus(404);
-        }
-        //   response.json(cityStats);
-        console.log(`New GET to /environment-stats/${city}/${year}`);
-    });
-
 
     app.put(BASE_API_URL + "/environment-stats/:city", (req, res) => {
         const city = stripAccents(req.params.city.toLowerCase());
@@ -224,6 +259,21 @@ module.exports = (app) => {
         console.log(`Error 405 Method not Allowed`);
 
     });
+
+
+    app.delete(BASE_API_URL + "/environment-stats", (req, res) => {
+        db.remove({}, { multi: true }, (err, numRemoved) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send(err);
+            } else {
+                console.log(`All ${numRemoved} environment stats from database`);
+                res.sendStatus(204);
+                environment_stats=[];
+            }
+        });
+    });
+    
 
     app.get(BASE_API_URL +"/environment-stats/docs", (req, res) => {
         console.log("Se ejecuta");
