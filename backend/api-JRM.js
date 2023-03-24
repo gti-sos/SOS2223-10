d2b2341c83dea3c336ce3a77898a2bf0be760470
Joaquín <joarodmar2@us.es>
2023-03-24 13:59:37 +0100

    commit actualizado casi final

--- backend/api-JRM.js
+++ backend/api-JRM.js
@@ -19,91 +19,141 @@ module.exports = (app) => {
     db.insert(economy_stats);
         
 
-    app.get(BASE_API_URL+"/economy-stats/loadInitialData", (request,response) => {
+    app.get(BASE_API_URL+"/economy-stats/loadInitialData", (req,res) => {
         console.log("New GET to /economy-stats/loadInitialData");
         db.find({}, function(err,data){
             if(err){
                 console.log(`Error geting /economy-stats/loadInitialData: ${err}`);
-                response.sendStatus(500);
+                res.sendStatus(500);
             }
             else{
                 if(data.length==0){
                     console.log(`data inserted: ${economy_stats.length}`);  
                     db.insert(economy_stats); 
-                    response.json(economy_stats.map((d)=>{
+                    res.json(economy_stats.map((d)=>{
                         delete d._id;
                         return d;
                     }));    
                 }
                 else{
                      console.log(`Data is already inserted: ${data.length}`);
-                     response.status(200).send("Data is already inserted");          
+                     res.status(200).send("Data is already inserted");          
                 }
             }
         });
     });
 
-    app.get(BASE_API_URL + "/economy-stats", (req,res)=>{ 
-        var query = req.query;
-        dbquery = {};
-        console.log("Peticion GET");
+    app.get(BASE_API_URL + "/economy-stats", (req, res) => {
+        const query = req.query;
+        const dbquery = {};
+        console.log("New GET to /economy-stats/");
         console.log(query.period);
-        var limit = Number.MAX_SAFE_INTEGER;
-        var offset = 0;
-        if(query.offset){
-            offset = parseInt(query.offset);
-            console.log(offset);
-            delete query.offset;
+      
+        let limit = Number.MAX_SAFE_INTEGER;
+        let offset = 0;
+
+        var from = req.query.from;
+        var to = req.query.to;
+
+        if(from>to){
+            console.log(`No se han recibido los campos esperados:`);
+            res.status(400).send("Bad Request");
         }
-        if(query.limit){
-            limit = parseInt(query.limit);
-            delete query.limit;
+      
+        if (query.offset) {
+          offset = parseInt(query.offset);
+          console.log(offset);
+          delete query.offset;
         }
-        if(query.period){
-            dbquery['period'] = parseInt(query.period);
-            console.log(offset);
+      
+        if (query.limit) {
+          limit = parseInt(query.limit);
+          delete query.limit;
         }
-        if(query.finished_house){
-            dbquery['finished_house'] = parseFloat(query.finished_house);
+      
+        if (query.territory) {
+          dbquery['territory'] = query.territory;
         }
-        if(query.half_price_m_two){
-            dbquery['half_price_m_two'] = parseFloat(query.half_price_m_two);
+      
+        if (query.period) {
+          dbquery['period'] = parseInt(query.period);
+          console.log(offset);
         }
-        if(query.tourist){
-            dbquery['tourist'] = parseFloat(query.tourist);
+      
+        if (query.finished_house) {
+          dbquery['finished_house'] = parseInt(query.finished_house);
         }
-        
+      
+        if (query.half_price_m_two) {
+          dbquery['half_price_m_two'] = parseInt(query.half_price_m_two);
+        }
+      
+        if (query.tourist) {
+          dbquery['tourist'] = parseInt(query.tourist);
+        }
         console.log(dbquery);
-        db.find(dbquery).skip(offset).limit(limit).exec((err,docs) =>{
-            console.log(docs);
-            if(err){
-                res.sendStatus(500);
-            }
-            else{
-                if(docs == 0){
-                    docs.forEach((data) => {
-                        delete data._id;
-                    });
-                    res.status(200).send(JSON.stringify(docs,null,2));
-                    //res.sendStatus(404);
-                }
-                else{
-                    docs.forEach((data) => {
-                        delete data._id;
-                    });
-                    res.status(200).send(JSON.stringify(docs,null,2));
-                }
-            }
-        })
+
         
+      
+        db.find(dbquery).skip(offset).limit(limit).exec((err, docs) => {
+          console.log(docs);
+
+          if(from != null && to != null){
+            docs = docs.filter((reg)=>
+            {
+                return (reg.period >= from && reg.period <=to);
+            });
+
+            if (docs==0){
+                console.log(`Data not found /economy-stats: ${err}`);
+                res.status(404).send("Data not found");
+            }    
+          }
+
+          if (err) {
+            res.sendStatus(500);
+          } else {
+            if (docs == 0) {
+              docs.forEach((data) => {
+                delete data._id;
+              });
+              res.status(200).send(JSON.stringify(docs, null, 2));
+              // res.sendStatus(404);
+            } else {
+              docs.forEach((data) => {
+                delete data._id;
+                });
+              res.status(200).send(JSON.stringify(docs, null, 2));
+            }
+            
+          }
+        });
     });
     
     app.get(BASE_API_URL+"/economy-stats/:territory", (request,response) => {
         var territorio = request.params.territory;
+        var from = request.query.from;
+        var to = request.query.to;
+        
+         console.log(`New GET to /economy-stats/${territorio}`);
 
-        console.log(`New GET to /economy-stats/${territorio}`);
+        if(from>to){
+            console.log(`No se han recibido los campos esperados:`);
+            response.status(400).send("Bad Request");
+        }
 
         db.find({territory : territorio}).skip(0).limit(economy_stats.length).exec((err, data) =>{
+            if(from != null && to != null){
+                data = data.filter((reg)=>
+                {
+                    return (reg.period >= from && reg.period <=to);
+                });
+    
+                if (data==0){
+                    console.log(`Data not found /economy-stats: ${err}`);
+                    res.status(404).send("Data not found");
+                }    
+            }
             if(err){
                 console.log(`Error geting /economy-stats/${territorio}: ${err}`);
                 response.sendStatus(500);
@@ -120,33 +170,28 @@ module.exports = (app) => {
                     response.status(404).send("Data not found");
                  }        
             }
+            
+
+
         });
     });
-    /*
-    app.get(BASE_API_URL+"/economy-stats/:period", (request,response) => {
-        var año = parseInt(request.params.period);
-        console.log(`New GET to /economy-stats/${año}`);
-        db.find({$and: [{period:año}]}).exec(function(err,data){
-            if(err){
-                console.log(`Error geting /economy-stats/${año}: ${err}`);
-                response.sendStatus(500);
-            }else{
-                if(data.length!= 0){
-                    console.log(`data returned ${data.length}`);
-                    response.json(data.map((d)=>{
-                        delete d._id;
-                        return d;
-                    }));
+
+    app.get(BASE_API_URL +"/economy-stats/:territory/:period", (req,res)=>{
+        var data = req.params; //parametro solicitado
+            
+        db.find({territory: data.territory, period: parseInt(data.period)}, (err, docs) => {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                if (docs == 0) {
+                    res.sendStatus(404);
+                } else {
+                    delete docs[0]._id;
+                    res.status(200).send(JSON.stringify(docs[0], null, 2));
                 }
-                 else{
-                    console.log(`Data not found /economy-stats/${año}: ${err}`);
-                    response.status(404).send("Data not found");
-                 }    
             }
-        })
-        
+        });
     });
-    */
 
     app.post(BASE_API_URL+"/economy-stats/:territory",(request,response)=>{
         response.sendStatus(405, "Method not allowed");
@@ -264,24 +309,6 @@ module.exports = (app) => {
             }
         });
     });
-
-    function paginacion(req, lista){
-
-        var res = [];
-        const limit = req.query.limit;
-        const offset = req.query.offset;
-        
-        if(limit < 1 || offset < 0 || offset > lista.length){
-            res.push("ERROR EN PARAMETROS LIMIT Y/O OFFSET");
-            return res;
-        }
-    
-        res = lista.slice(offset,parseInt(limit)+parseInt(offset));
-        return res;
-    
-    };
-
-    
 
 }
 
