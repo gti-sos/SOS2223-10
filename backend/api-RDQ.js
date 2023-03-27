7ef5e162cfc89903e82eb58f570c597069095e31
Rafael Diaz <rafdiaqui>
2023-03-27 18:01:53 +0200

    commit final

--- backend/api-RDQ.js
+++ backend/api-RDQ.js
@@ -56,7 +56,7 @@ app.get(BASE_API_URL+"/employment-stats/loadInitialData", (req,res) => {
 });
 
 
-
+//
 app.get(BASE_API_URL + "/employment-stats", (req,res)=>{ 
     var query = req.query;
     dbquery = {};
