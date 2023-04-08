cd04e6a2cb4dd86af14a52958f4fbcaefc4ed241
Joaquín <joarodmar2@us.es>
2023-04-08 13:42:07 +0200

    actualiz

---
+++ frontend/src/routes/Header.svelte
@@ -0,0 +1,22 @@
+<main>
+    <nav>
+        <div class="container">
+            <a href="/">Home</a>
+            <a href="/economy-stats">Economy stats</a>
+            <a href="/environment-stats">Environment stats</a>
+            <a href="/employment-stats">Employment stats</a>          
+        </div>
+    </nav>
+</main>
+<style>
+    a {
+        color : rgb(2, 56, 107);
+        text-decoration: none;
+        font-size: 20px;
+    }
+    .container {
+        display: grid;
+        grid-template-columns: 25% 25% 25% 25%;
+        text-align: center;
+    }
+</style>
