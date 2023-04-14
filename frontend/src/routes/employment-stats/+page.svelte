cd9a93e0b7a1097a2e3390ef2360fbdbfc2d757e
Rafael Diaz <rafdiaqui>
2023-04-14 11:33:13 +0200

    a

--- frontend/src/routes/employment-stats/+page.svelte
+++ frontend/src/routes/employment-stats/+page.svelte
@@ -1,118 +1,288 @@
+<main>
+    <h1>
+        Bienvenido a la API employment_stats 
+    </h1>
+    <div style="text-align: center;">
+    <h5>Desarrollada por Rafael Diaz Quintero</h5>
+    </div>
+</main>
+
+
+
 <script>
     // @ts-nocheck
+    
         import { onMount } from "svelte";
         import { dev } from "$app/environment";
-        import { Button, Table } from 'sveltestrap';
-        onMount(async () =>{
-            getFile();
+        import { Button, Table, ButtonToolbar } from "sveltestrap";
+        onMount(async () => {
+            getEmployment();
         });
-        let API = "/api/v2/employment-stats";
-        let mensaje = "";
+        
+        let API = '/api/v2/employment-stats';
+        let mensajeUsuario = "";
+        
         if(dev)
-            API = "http:/localhost:12345"+API
+            API = 'http://localhost:12345'+API
             
         let employment_stats = [];
-        let newProvince = "province";
-        let newPeriod = "period";
-        let newpopulation_over_16_years = "population_over_16_years";
-        let newactivity_men_percentage = "activity_men_percentage";
-        let newactivity_women_percentage = "activity_women_percentage";
+        let newPeriod = '';
+        let newProvince = '';
+        let newpopulation_over_16_years = '';
+        let newactivity_men_percentage = '';
+        let newactivity_women_percentage = '';
+        
+    
         let result = "";
         let resultStatus = "";
-        async function getFile(){
+        async function loadData() {
+            resultStatus = result = "";
+            const res = await fetch(API+'/loadInitialData', {
+                method: 'GET'
+            });
+            const status = await res.status;
+            resultStatus = status;
+            if(status==201){
+                getEmployment(); 
+            }	
+}
+    
+        async function getEmployment () {
             resultStatus = result = "";
             const res = await fetch(API, {
-                method: "GET"
+                method: 'GET'
             });
             try{
                 const data = await res.json();
-                result = JSON.stringify(data, null, 2);
+                result = JSON.stringify(data,null,2);
                 employment_stats = data;
             }catch(error){
-                console.log(`Error parseando el resultado: ${error}`);
+                console.log(`Error parsing result: ${error}`);
             }
             const status = await res.status;
             resultStatus = status;
         }
+   
         let insertedData = [];
-        async function createFile(){
+        async function createEmployment (){
             resultStatus = result = "";
-            const newFile = {
-                province: newprovince,
+            const newEmployment = {
+                province: newProvince,
                 period: parseInt(newPeriod),
                 population_over_16_years: parseInt(newpopulation_over_16_years),
-                activity_men_percentage: parseInt(newactivity_men_percentage),
-                activity_women_percentage: parseInt(newactivity_women_percentage)
+                activity_men_percentage: parseFloat(newactivity_men_percentage),
+                activity_women_percentage: parseFloat(newactivity_women_percentage),
+         
             };
-    
-            
+            // Comprobar si el nuevo dato ya ha sido insertado previamente
             const existingData = insertedData.find(data => 
-                data.province === newprovince && data.period === newPeriod && data.newpopulation_over_16_years === newpopulation_over_16_years
-                    && data.activity_men_percentage === newactivity_men_percentage && data.activity_women_percentage === newactivity_women_percentage
+                data.period === newperiod && data.province === newProvince && data.population_over_16_years === newPopulation_over_16_years
+                    && data.activity_men_percentage === newactivity_men_percentage && data.activity_women_percentage === newactivity_women_percentage 
             );
             if (existingData) {
-                mensaje = "Ya existe ese dato";
+                mensajeUsuario = "Ya existe ese dato";
                 return;
             }
-    
             const res = await fetch(API, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newFile)
+                method: 'POST',
+                headers:{
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify(
+                    newEmployment
+                )     
             });
-    
             const status = await res.status;
             resultStatus = status;
             if (status == 201) {
-                getFile();
-                mensaje = "Se ha creado el nuevo dato introducido";
-                insertedData.push(newFile);
+                getEmployment();
+                mensajeUsuario = "Se ha creado el nuevo dato introducido";
+                insertedData.push(newEmployment);
             } else if (status == 409) {
-                mensaje = "El dato introducido ya existe";
-                getFile();
+                mensajeUsuario = "El dato introducido ya existe";
+                getEmployment();
             } else if (status == 400) {
-                mensaje = "Las propiedades introducidas no tienen un formato correcto";
-                getFile();
+                mensajeUsuario = "Las propiedades introducidas no tienen un formato correcto";
+                getEmployment();
             } else {
-                mensaje = "No se ha podido crear el dato introducido";
-                getFile();
-            }     
-        }
-          
-        async function deleteAll(){
+                mensajeUsuario = "No se ha podido crear el dato introducido";
+                getEmployment();
+            }
+        }   
+        async function deleteEmployment(Province,Period){
             resultStatus = result = "";
-            const res = await fetch(API, {
+            const res = await fetch(API+"/"+Province+"/"+Period, {
                 method: "DELETE"
             });
             const status = await res.status;
             resultStatus = status;
-            if(status==200 || status == 204){
-                await getFile();
-                mensaje = "Se han borrado correctamente los datos";
-            }else{
-                mensaje = "No se han podido borrar los datos";
+            if(status==200){
+                getEmployment ();
+                mensajeUsuario = "Recurso borrado";
+            }else if(status==500){
+                mensajeUsuario = "Error cliente";
+            }else if(status==404){
+                getEmployment ();
+                mensajeUsuario = "No se ha encontrado ese recurso";
             }
         }
+        async function deleteEmploymentAll () {
+            resultStatus = result = "";
+            const res = await fetch(API, {
+                method: 'DELETE'
+            });
+            const status = await res.status;
+            resultStatus = status;
+            if(status==200 || status == 204){
+                await getEmployment();
+                mensajeUsuario = "Se han borrado correctamente los datos";
+            }else{
+                mensajeUsuario = "No se han podido borrar los datos";
+            }		
+        }
+        async function searchEmployment() {
+    resultStatus = result = "";
+    const res = await fetch(`${API}/${newProvince}/${newPeriod}`, {
+      method: 'GET'
+    });
+    const status = await res.status;
+    resultStatus = status;
+    if (status == 200) {
+      const data = await res.json();
+      employment_stats = [data]; // Actualizar la lista con el resultado de búsqueda
+      mensajeUsuario = `Resultado de búsqueda para ${newProvince} en ${newPeriod}`;
+    } else if (status == 404) {
+      employment_stats = []; // Limpiar la lista si no se encuentra ningún resultado
+      mensajeUsuario = `No se encontraron resultados para ${newProvince} en ${newPeriod}`;
+    } else {
+      mensajeUsuario = "No se ha podido realizar la búsqueda";
+    }
+  }   
+  export let total;
+  // Paginación
+
+
     </script>
 
-    <h1 style="text-align: center; font-family:'Times New Roman', Times, serif; font-size: 60px;">Datos employment_stats</h1>
-    <p></p>
-    {#if mensaje !=""}
-    <h2 style="color: red; text-align: center; font-family:Arial, Helvetica, sans-serif">{mensaje}</h2>
-    {/if}
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos Empleo</title>
+    
+        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+<style>
+body {
+    font-family: 'Roboto';
+    font-size: 18px;
+}
+        h1 {
+            text-align: center;
+            font-size: 40px;
+            margin-top: 50px;
+        }
+        .botones {
+            margin-bottom: 30px;
+        }
+        h2 {
+            text-align: center;
+            font-family: Arial, Helvetica, sans-serif;
+            color: blue;
+            margin-bottom: 20px;
+        }
+        table {
+            margin: 0 auto;
+            width: 80%;
+            border-collapse: collapse;
+            margin-bottom: 30px;
+        }
+        th, td {
+            text-align: center;
+            padding: 10px;
+            border: 1px solid #ddd;
+        }
+        th {
+            background-color: #f2f2f2;
+            font-weight: bold;
+        }
+        input {
+            padding: 5px;
+            width: 100%;
+            box-sizing: border-box;
+        }
+        .button {
+            display: inline-block;
+            padding: 10px;
+            border-radius: 5px;
+            border: none;
+            color: #fff;
+            background-color: #007bff;
+            text-decoration: none;
+            font-size: 16px;
+            margin-right: 10px;
+        }
+        .button:hover {
+            background-color: #0069d9;
+        }
+        .button.delete {
+            background-color: #dc3545;
+        }
+        .button.delete:hover {
+            background-color: #c82333;
+        }
+        .button-toolbar {
+            margin-bottom: 20px;
+        }
+        .alert {
+            text-align: center;
+            padding: 10px;
+            background-color: #f8d7da;
+            color: #721c24;
+            margin-bottom: 20px;
+            border: 1px solid #f5c6cb;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <h2>Datos Empleo</h2>
+    <div class="botones">
+        <div style="text-align: center;">
+        <div class="button-toolbar">
+            <button class="button" on:click={loadData}>Datos Empleo</button>
+        </div>
+        {#if mensajeUsuario !=""}
+            <div class="alert">{mensajeUsuario}</div>
+        {/if}
+    </div>
+</div>
+<form on:submit|preventDefault={searchEmployment} class="p-3 border rounded">
+    <div class="form-group">
+      <label for="province" class="font-weight-bold">Provincia:</label>
+      <input type="text" class="form-control form-control-sm" id="province" bind:value={newProvince} required>
+    </div>
+    <div class="form-group">
+      <label for="period" class="font-weight-bold">Period:</label>
+      <input type="number" class="form-control form-control-sm" id="period" bind:value={newPeriod} required>
+    </div>
+    <Button color="primary" type="submit" class="mt-3">Buscar</Button>
+  </form>
+<div>
+  <button class="button" on:click={getEmployment}>Obtener Datos Empleo</button>
+</div>
 
-    <Table striped>
+  
+  
+    <table>
         <thead>
-          <tr>
-            <th style="text-decoration: underline;">Provincia:</th>
-            <th style="text-decoration: underline;">Periodo:</th>
-            <th style="text-decoration: underline;">Poblacion mayor de 16 años:</th>
-            <th style="text-decoration: underline;">Porcentaje actividad en hombres:</th>
-            <th style="text-decoration: underline;">Porcentaje actividad en mujeres:</th>
-            <th style="text-decoration: underline;">Acción:</th>
-          </tr>
+            <tr>
+                <th>Provincia</th>
+                <th>Periodo</th>
+                <th>Poblacion mayor de edad</th>
+                <th>Porcentaje de actividad en hombres</th>
+                <th>Porcentaje de actividad en mujeres</th>
+                <th>Acciones</th>
+            </tr>
         </thead>
         <tbody>
             <tr>
@@ -121,28 +291,44 @@
                 <td><input bind:value={newpopulation_over_16_years}></td>
                 <td><input bind:value={newactivity_men_percentage}></td>
                 <td><input bind:value={newactivity_women_percentage}></td>
-                <td><Button color="success" on:click={createFile}>Crear</Button></td>
+                <td><button class="button" on:click={createEmployment}>Crear</button></td>
             </tr>
-        
-        {#each employment_stats as employment }
-          <tr>
-            <td><a href="/employment/{employment.province}/{employment.period}">{employment.province}</a></td>
-            <td>{employment.period}</td>
-            <td>{employment.newpopulation_over_16_years}</td>
-            <td>{employment.activity_men_percentage}</td>
-            <td>{employment.activity_women_percentage}</td>
-          </tr>
-        {/each}
+            {#each employment_stats as Employment}
+            <tr>
+              <td>{Employment.province}</td>
+              <td>{Employment.period}</td>
+              <td>{Employment.population_over_16_years}</td>
+              <td>{Employment.activity_men_percentage}</td>
+              <td>{Employment.activity_women_percentage}</td>
+            
+              <td>
+                <Button color="info" class="btn-sm">
+                  <a href='employment-stats/{Employment.province}/{Employment.period}' class="text-white text-decoration-none">Editar</a>
+                </Button>
+              </td>
+              <td>
+                <Button color="danger" class="btn-sm" on:click={deleteEmployment(Employment.province,Employment.period)}>
+                  Borrar
+                </Button>
+              </td>
+            </tr>
+          {/each}
+          
+          <div class="mt-3">
+            <Button color="danger" on:click={deleteEmploymentAll} class="btn-sm">
+              Borrado de Datos
+            </Button>
+          </div>
         </tbody>
-    </Table>
-    
-    <div style="text-align: center;">
-        <Button color="danger" on:click={deleteAll}>Borrar Datos</Button>
-    </div>
-    
+    </table>
+</body>
+</html>
+          
+
+     <!--
     {#if resultStatus != ""}
         <p>
-            <strong>Número de datos: {employment_stats.length}</strong>
+            <strong>Número de datos: {economy_stats.length}</strong>
         </p>
         <strong>Result:</strong>
         <pre>
@@ -150,3 +336,5 @@
 {result}
         </pre>
     {/if}
+    -->
+   
--- frontend/src/routes/employment-stats/[province]/[period]/+page.svelte
+++ frontend/src/routes/employment-stats/[province]/[period]/+page.svelte
@@ -1,202 +1,196 @@
 <script>
-    // @ts-nocheck
-    
-        import { onMount } from 'svelte';
-        import { dev } from '$app/environment';
-        import { Alert, Column, Button, Table } from 'sveltestrap';
-        import { page } from '$app/stores';
-    let search = false; // se ha buscado
-        onMount(async () => {
-            getEnvironment();
-        });
-        let province = $page.params.province;
-        let period = $page.params.period;
-        console.log(province);
-        console.log(period);
-        let API = '/api/v2/environment-stats/' + province + "/" + period;
-        let msgVisible=false;
-        let checkMSG="";
-        let color="success";
-        
-        if(dev)
-            API = 'http://localhost:12345'+API
-            
-
-        let uptprovince = 'province';
-        let uptperiod = 'period';
-        let population_over_16_years = 'population_over_16_years';
-        let activity_men_percentage = 'activity_men_percentage';
-        let activity_women_percentage = 'activity_women_percentage';
-       
-        
-        let message = "";
-    
-        let result = "";
-        let resultStatus = "";
-    
-        async function getEnvironment () {
-            resultStatus = result = "";
-            const res = await fetch(API, {
-                method: 'GET'
-            });
-            try{
-                const data = await res.json();
-                result = JSON.stringify(data,null,2);
-                uptprovince = data.province;
-                uptperiod = data.period;
-                population_over_16_years = data.population_over_16_years;
-                activity_men_percentage = data.activity_men_percentage;
-                activity_women_percentage = data.activity_women_percentage;
-              
-                
-            }catch(error){
-                console.log(`Error parsing result: ${error}`);
-            }
-            const status = await res.status;
-            resultStatus = status;	
-        }
+  // @ts-nocheck
+  
+      import { onMount } from 'svelte';
+      import { dev } from '$app/environment';
+      import { Alert, Column, Button, Table } from 'sveltestrap';
+      import { page } from '$app/stores';
+  let search = false; // se ha buscado
+      onMount(async () => {
+        getEmployment();
+      });
+      let province = $page.params.province;
+      let period = $page.params.period;
+      console.log(province);
+      console.log(period);
+      let API = '/api/v2/environment-stats/' + province + "/" + period;
+      let msgVisible=false;
+      let checkMSG="";
+      let color="success";
       
-        async function updateEmployment () {
-    resultStatus = result = "";
-    try {
-        const res = await fetch(API, {
-            method: 'PUT',
-            headers:{
-                "Content-Type" : "application/json"
-            },
-            body:JSON.stringify({
-                province: String(uptprovince),
-                period: Number(uptperiod),
-                population_over_16_years: Number(population_over_16_years),
-                activity_men_percentage: Number(activity_men_percentage),
-                activity_women_percentage: Number(activity_women_percentage),
-               
-            })
-        });
-        const status = await res.status;
-        resultStatus = status;
-        if(status==200){
-            msgVisible = true;
-            color = "success";
-            checkMSG = "Datos empleo actualizados";
-            getEnvironment();
-        } else {
-            msgVisible = true;
-            color = "danger";
-            checkMSG = "Error al actualizar los datos de empleo, comprueba que hayas completado todos los campos y que los campos numericos son mayores que 0 y vuelve a intentarlo";
-        }
-    } catch (error) {
-        console.error(error);
-        msgVisible = true;
-        color = "danger";
-        checkMSG = "Hubo un error al intentar actualizar los datos de empleo, intenta nuevamente más tarde";
-    }
-}
-        
+      if(dev)
+ 
+   
+          API = 'http://localhost:12345'+API
+          
+      let uptProvince = 'Provincia';
+      let uptPeriod = 'Periodo';
+      let population_over_16_years = 'Poblacion Mayor de edad';
+      let activity_men_percentage = 'Porcentaje de actividad en hombres';
+      let activity_women_percentage = 'Porcentaje de actividad en mujeres';
+     
+      
+      let message = "";
+      let result = "";
+      let resultStatus = "";
+  
+      async function getEmployment () {
+          resultStatus = result = "";
+          const res = await fetch(API, {
+              method: 'GET'
+          });
+          try{
+              const data = await res.json();
+              result = JSON.stringify(data,null,2);
+              uptProvince = data.province;
+              uptPeriod = data.period;
+              population_over_16_years = data.population_over_16_years;
+              activity_men_percentage = data.activity_men_percentage;
+              activity_women_percentage = data.activity_women_percentage;
+            
+              
+          }catch(error){
+              console.log(`Error parsing result: ${error}`);
+          }
+          const status = await res.status;
+          resultStatus = status;	
+      }
     
-    </script>
+      async function updateEmployment () {
+  resultStatus = result = "";
+  try {
+      const res = await fetch(API, {
+          method: 'PUT',
+          headers:{
+              "Content-Type" : "application/json"
+          },
+          body:JSON.stringify({
+              province: String(uptCity),
+              period: Number(uptYear),
+              population_over_16_years: Number(population_over_16_years),
+              activity_men_percentage: Number(activity_men_percentage),
+              activity_women_percentage: Number(activity_women_percentage),
+             
+          })
+      });
+      const status = await res.status;
+      resultStatus = status;
+      if(status==200){
+          msgVisible = true;
+          color = "success";
+          checkMSG = "Datos Medio Ambiente actualizados";
+          getEmployment();
+      } else {
+          msgVisible = true;
+          color = "danger";
+          checkMSG = "Error al actualizar los datos de medio ambiente, comprueba que hayas completado todos los campos y que los campos numericos son mayores que 0 y vuelve a intentarlo";
+      }
+  } catch (error) {
+      console.error(error);
+      msgVisible = true;
+      color = "danger";
+      checkMSG = "Hubo un error al intentar actualizar los datos de medio ambiente, intenta nuevamente más tarde";
+  }
+}
+      
+  
+  </script>
 
 <style>
-  table {
-    border-collapse: collapse;
-    width: 100%;
-    font-family: Arial, sans-serif;
-    font-size: 14px;
-    text-align: left;
-  }
-  
-  th, td {
-    padding: 10px;
-    border: 1px solid #ddd;
-  }
-  
-  th {
-    background-color: #f2f2f2;
-    font-weight: bold;
-  }
-  
-  tr:hover {
-    background-color: #f5f5f5;
-  }
-  
-  body {
+table {
+  border-collapse: collapse;
+  width: 100%;
+  font-family: Arial, sans-serif;
+  font-size: 14px;
+  text-align: left;
+}
+
+th, td {
+  padding: 10px;
+  border: 1px solid #ddd;
+}
+
+th {
+  background-color: #f2f2f2;
+  font-weight: bold;
+}
+
+tr:hover {
+  background-color: #f5f5f5;
+}
+
+body {
 margin: 0 20px; /* Cambia el valor de 20px según tus necesidades */
 }
 .page-container {
-        margin: 0 50px; /* aplicar margen de 50px en los laterales */
-      }
-      h1 {
-        font-size: 36px;
-        font-weight: bold;
-        text-align: center;
-        color: #333;
-        text-transform: uppercase;
-        letter-spacing: 2px;
-        font-weight: normal;
-      } 
-  
+      margin: 0 50px; /* aplicar margen de 50px en los laterales */
+    }
+    h1 {
+      font-size: 36px;
+      font-weight: bold;
+      text-align: center;
+      color: #333;
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      font-weight: normal;
+    } 
+
 </style>
 
 
-    <body>
-      <div class="page-container">
-    <h1>Dato ciudad y Año en concreto</h1>
-    
-    
-       
-    
-    
-    
-      
-    <Alert color={color} isOpen={msgVisible} toggle={() => (msgVisible = false)}>
-      {#if checkMSG!=""}
-        {checkMSG}
-      {/if}
-    </Alert>
-      
-    <table class="table table-bordered table-striped table-hover">
-      <thead>
-        <tr>
-          <th width="115px">province</th>
-          <th>period</th>
-          <th width="89px">population_over_16_years</th>
-          <th width="89px">activity_men_percentage</th>
-          <th>activity_women_percentage</th>
-          
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          
-          <td>{uptprovince}</td>
-
-          <td><input type="text" bind:value={uptperiod} class="form-control"></td>
-          <td><input type="text" bind:value={population_over_16_years} class="form-control"></td>
-          <td><input type="text" bind:value={activity_men_percentage} class="form-control"></td>
-          <td><input type="text" bind:value={activity_women_percentage} class="form-control"></td>
-          
-          <td><Button on:click={updateEmployment} class="bg-warning font-weight-bold">Editar</Button></td>
-        </tr>
-      </tbody>
-    </table>
-    
-      
-      
-
-      {#if message != ""}
-        <h1 style="color :red">{message}</h1>
+  <body>
+    <div class="page-container">
+  <h1>Dato ciudad y Año en concreto</h1>
+  
+  <Alert color={color} isOpen={msgVisible} toggle={() => (msgVisible = false)}>
+    {#if checkMSG!=""}
+      {checkMSG}
     {/if}
+  </Alert>
+    
+  <table class="table table-bordered table-striped table-hover">
+    <thead>
+      <tr>
+        <th width="115px">Provincia</th>
+        <th>Periodo</th>
+        <th width="89px">Poblacion mayor de edad</th>
+        <th width="89px">Porcentaje de actividad en hombres</th>
+        <th>Porcentaje de actividad en mujeres</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        
+        <td>{uptProvince}</td>
+        <td><input type="text" bind:value={uptPeriod} class="form-control"></td>
+        <td><input type="text" bind:value={population_over_16_years} class="form-control"></td>
+        <td><input type="text" bind:value={activity_men_percentage} class="form-control"></td>
+        <td><input type="text" bind:value={activity_women_percentage} class="form-control"></td>
+        
+        <td><Button on:click={updateEmployment} class="bg-warning font-weight-bold">Editar</Button></td>
+      </tr>
+    </tbody>
+  </table>
+  
+    
+    
 
-      
-    {#if resultStatus != ""}
-        <p>
-            Result:
-        </p>
-        <pre>
+    {#if message != ""}
+      <h1 style="color :red">{message}</h1>
+  {/if}
+
+    
+  {#if resultStatus != ""}
+      <p>
+          Result:
+      </p>
+      <pre>
 {resultStatus}
 {result}
-        </pre>
-    {/if}
-  </div>
-    </body>
+      </pre>
+  {/if}
+</div>
+  </body>
+  
   
