94866b6e17ce9cb293a68f32f09bf92dd3e87a96
Joaquín <joarodmar2@us.es>
2023-04-16 11:04:40 +0200

    pequeño arreglo

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const BASE_API_URL_v1 = "/api/v1";
 
 import express from "express";
 import cors from "cors";
+import {loadBackend_JRM_v1} from "./backend/api-JRM.js";
 import {loadBackend_RDQ_v2} from "./backend/api-RDQ-v2.js";
 import {loadBackend_JRM_v2} from "./backend/api-JRM-v2.js";
 import {loadBackend_RPP_v2} from "./backend/api-RPP-v2.js";
+import {loadBackend_RPP_v3} from "./backend/api-RPP-v3.js";
 //var backend = require("./backend/api-JRM");
 //var backend2 = require("./backend/api-RDQ");
 //var backend3 = require("./backend/api-RPP");
@@ -24,6 +26,12 @@ app.use(express.json());
 app.get(BASE_API_URL + "/economy-stats/docs", (req, res) => {
     console.log("Se ejecuta" + BASE_API_URL + "/economy-stats/docs");
     res.redirect("https://documenter.getpostman.com/view/25969218/2s93JzMgXV");
+
+});
+
+app.get(BASE_API_URL_v1 + "/economy-stats/docs", (req, res) => {
+    console.log("Se ejecuta" + BASE_API_URL_v1 + "/economy-stats/docs");
+    res.redirect("https://documenter.getpostman.com/view/25969218/2s93XyThts");
 
 });
 
