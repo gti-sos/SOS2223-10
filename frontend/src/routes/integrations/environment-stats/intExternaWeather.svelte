c17c638fa91971226bdc00b8a692c45bdbdf1e89
ruspat1 <ruspat1@us.es>
2023-05-19 14:17:58 +0200

    19-05

--- frontend/src/routes/integrations/environment-stats/intExternaWeather.svelte
+++ frontend/src/routes/integrations/environment-stats/intExternaWeather.svelte
@@ -1,95 +1,120 @@
 <script>
-    // @ts-nocheck
+  // @ts-nocheck
 
-    import * as echarts from "echarts";
-    import { onMount } from "svelte";
-    import { dev } from "$app/environment";
-    import axios from "axios";
+  import * as echarts from "echarts";
+  import { onMount } from "svelte";
+  import { dev } from "$app/environment";
+  import axios from "axios";
 
-    let API = "/api/v3/environment-stats";
-    if (dev) API = "http://localhost:12345" + API;
+  let API = "/api/v3/environment-stats";
+  if (dev) API = "http://localhost:12345" + API;
 
-    let result = "";
-    let resultStatus = "";
-    let apiData = "";
-    let weatherData = "";
+  let result = "";
+  let resultStatus = "";
+  let apiData = "";
 
-    let API_URL =
-        "https://sos2223-10.ew.r.appspot.com/api/v3/environment-stats";
+  const cityCoordinates = {
+  Cordoba: {
+    latitude: 37.888175,
+    longitude: -4.779383,
+  },
+  Sevilla: {
+    latitude: 37.389092,
+    longitude: -5.984459,
+  },
+  Jaen: {
+    latitude: 37.779594,
+    longitude: -3.784909,
+  },
+  Granada: {
+    latitude: 37.176487,
+    longitude: -3.597929,
+  },
+  Malaga: {
+    latitude: 36.721273,
+    longitude: -4.421399,
+  },
+  Huelva: {
+    latitude: 37.261421,
+    longitude: -6.944722,
+  },
+  Almeria: {
+    latitude: 36.83814,
+    longitude: -2.459743,
+  },
+  Cadiz: {
+    latitude: 36.527061,
+    longitude: -6.288596,
+  },
+};
 
-    /* async function loadEchart(apiData, weatherData) {
-        // Crear un arreglo con las etiquetas del eje X
-        const labels = apiData.map((datum) => datum.city);
-
-        // Crear un arreglo con los valores del eje Y (protección de espacios)
-        const protectedSpaces = apiData.map((datum) => datum.protected_space);
-
-        // Crear un arreglo con los valores del eje Y (temperaturas)
-        // const temperatures = weatherData.map((datum) => datum.current.temp_c);
-        const temperatures = [weatherData.current.temp_c];
-
-        // Crear una instancia de ECharts
-        const chart = echarts.init(document.getElementById("chart"));
-
-        // Configurar opciones de la gráfica
-        const options = {
-            title: {
-                text: "Espacios protegidos vs Temperaturas",
-            },
-            tooltip: {
-                trigger: "axis",
-            },
-            legend: {
-                data: ["Espacios protegidos", "Temperaturas"],
-            },
-            xAxis: {
-                type: "category",
-                data: labels,
-            },
-            yAxis: [
-                {
-                    type: "value",
-                    name: "Espacios protegidos",
-                },
-                {
-                    type: "value",
-                    name: "Temperaturas (°C)",
-                },
-            ],
-            series: [
-                {
-                    name: "Espacios protegidos",
-                    type: "bar",
-                    data: protectedSpaces,
-                },
-                {
-                    name: "Temperaturas",
-                    type: "line",
-                    yAxisIndex: 1,
-                    data: temperatures,
-                },
-            ],
-        };
-
-        // Usar las opciones para generar la gráfica
-        chart.setOption(options);
-    } */
-
-    async function loadEchart(apiData, weatherData) {
-  // Obtener el elemento HTML donde se mostrará la gráfica
-  const chartElement = document.getElementById("chart");
   
+
+  let API_URL = "https://sos2223-10.ew.r.appspot.com/api/v3/environment-stats";
+
+  async function loadEchart(apiData, weatherData) {
+    // Obtener el elemento HTML donde se mostrará la gráfica
+    const chartElement = document.getElementById("chart");
+
+    // Crear la instancia de ECharts
+    const chart = echarts.init(chartElement);
+
+    // Configurar las opciones de la gráfica
+    const options = {
+      title: {
+        text: "Datos de ciudad, incendios y temperatura",
+      },
+      tooltip: {},
+      legend: {
+        data: ["Incendios", "Temperatura"],
+      },
+      xAxis: {
+        data: apiData.map((data) => data.city),
+      },
+      yAxis: [
+        {
+          name: "Incendios",
+          type: "value",
+        },
+        {
+          name: "Temperatura",
+          type: "value",
+        },
+      ],
+      series: [
+        {
+          name: "Incendios",
+          type: "bar",
+          data: apiData.map((data) => data.fire),
+        },
+        {
+          name: "Temperatura",
+          type: "line",
+          yAxisIndex: 1,
+          data: apiData.map((data) => weatherData[data.city]?.current?.temp_c),
+        },
+      ],
+    };
+
+    // Mostrar la gráfica
+    chart.setOption(options);
+  }
+
+  async function loadEchart2(apiData,airData) {
+  // Obtener el elemento HTML donde se mostrará la gráfica
+  const chartElement = document.getElementById("chart2");
+
   // Crear la instancia de ECharts
   const chart = echarts.init(chartElement);
-  
+
   // Configurar las opciones de la gráfica
   const options = {
     title: {
-      text: "Datos de ciudad, incendios y temperatura",
+      text: "Datos de ciudad, incendios, PM10 y PM25",
     },
     tooltip: {},
     legend: {
-      data: ["Incendios", "Temperatura"],
+      data: ["Incendios", "PM10", "PM25"],
     },
     xAxis: {
       data: apiData.map((data) => data.city),
@@ -100,82 +125,159 @@
         type: "value",
       },
       {
-        name: "Temperatura",
+        name: "PM",
         type: "value",
       },
     ],
     series: [
       {
         name: "Incendios",
-        type: "bar",
-        data: apiData.map((data) => data.fire),
+        type: "scatter",
+        symbolSize: 10,
+        data: apiData.map((data) => [data.city, data.fire]),
       },
       {
-        name: "Temperatura",
-        type: "line",
+        name: "PM10",
+        type: "scatter",
+        symbolSize: 10,
         yAxisIndex: 1,
-        data: apiData.map((data) => weatherData[data.city]?.current?.temp_c),
+        data: apiData.map((data) => [data.city, airData[data.city]?.data[0]?.pm10]),
+      },
+      {
+        name: "PM25",
+        type: "scatter",
+        symbolSize: 10,
+        yAxisIndex: 1,
+        data: apiData.map((data) => [data.city, airData[data.city]?.data[0]?.pm25]),
       },
     ],
   };
-  
+
   // Mostrar la gráfica
   chart.setOption(options);
 }
 
 
-    async function getData() {
-        resultStatus = result = "";
 
-        // Llamada a la API original
-        const res = await fetch(API, {
-            method: "GET",
-        });
-        try {
-            const dataReceived = await res.json();
-            result = JSON.stringify(dataReceived, null, 2);
-            apiData = dataReceived;
-        } catch (error) {
-            console.log(`Error parseando el resultado de la API: ${error}`);
-        }
+  
 
-        const weatherData = {};
-        for (const cityData of apiData) {
-            const options = {
-                method: "GET",
-                url: "https://weatherapi-com.p.rapidapi.com/current.json",
-                params: { q: `${cityData.city}` },
-                headers: {
-                    "X-RapidAPI-Key":
-                        "e52c3722d9msha0d5beb871b5cd4p1175cbjsnfae869968c4f",
-                    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-                },
-            };
-            try {
-                const weatherResponse = await axios.request(options);
-                const weatherDataForCity = weatherResponse.data;
-                weatherData[cityData.city] = weatherDataForCity;
-            } catch (error) {
-                console.log(
-                    `Error obteniendo el clima para ${cityData.city}: ${error}`
-                );
-            }
-        }
-        loadEchart(apiData, weatherData);
+ 
+
+  async function getData() {
+    resultStatus = result = "";
+
+    // Llamada a la API original
+    const res = await fetch(API, {
+      method: "GET",
+    });
+    try {
+      const dataReceived = await res.json();
+      result = JSON.stringify(dataReceived, null, 2);
+      apiData = dataReceived;
+    } catch (error) {
+      console.log(`Error parseando el resultado de la API: ${error}`);
     }
 
-    onMount(async () => {
-        getData();
+    const weatherData = {};
+    for (const cityData of apiData) {
+      const options = {
+        method: "GET",
+        url: "https://weatherapi-com.p.rapidapi.com/current.json",
+        params: { q: `${cityData.city}` },
+        headers: {
+          "X-RapidAPI-Key":
+            "e52c3722d9msha0d5beb871b5cd4p1175cbjsnfae869968c4f",
+          "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+        },
+      };
+      try {
+        const weatherResponse = await axios.request(options);
+        const weatherDataForCity = weatherResponse.data;
+        weatherData[cityData.city] = weatherDataForCity;
+      } catch (error) {
+        console.log(
+          `Error obteniendo el clima para ${cityData.city}: ${error}`
+        );
+      }
+    }
 
-    });
+    const airData = {};
+for (const cityData of apiData) {
+  const city = cityData.city;
+  const coordinates = cityCoordinates[city];
+  
+  if (coordinates) {
+    const lat = coordinates.latitude;
+    const lon = coordinates.longitude;
+    
+    const options = {
+      method: 'GET',
+      url: 'https://air-quality.p.rapidapi.com/current/airquality',
+      params: {
+        lon: lon,
+        lat: lat
+      },
+      headers: {
+      //  'X-RapidAPI-Key': 'e52c3722d9msha0d5beb871b5cd4p1175cbjsnfae869968c4f',
+      'X-RapidAPI-Key':'dff350a5e9msh0576314cd591864p1be088jsn092c880c0a12',
+        'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const airResponse = await axios.request(options);
+        const airDataForCity = airResponse.data;
+        airData[cityData.city] = airDataForCity;
+        console.log(airData);
+    } catch (error) {
+      console.error(`Error obteniendo la calidad del aire para ${city}: ${error}`);
+    }
+  } else {
+    console.warn(`No se encontraron coordenadas para ${city}.`);
+  }
+}
+
+
+    
+    console.log("a");
+        loadEchart(apiData, weatherData);
+     //   loadEchart2(apiData,airData);
+  }
+
+  onMount(async () => {
+    getData();
+  });
 </script>
 
 <svelte:head>
-    <script
-        src="https://cdn.jsdelivr.net/npm/echarts@5.2.2/dist/echarts.min.js"
-    ></script>
+  <script
+    src="https://cdn.jsdelivr.net/npm/echarts@5.2.2/dist/echarts.min.js"
+  ></script>
 </svelte:head>
 
 <main>
-    <div id="chart" style="width: 1300px;height:400px;" />
+  <div id="chart" style="width: 1300px;height:400px;" />
+  <div id="chart2" style="width: 1300px;height:400px;" />
 </main>
+
+<!-- Cordoba = 9ZMfhpfTHFfN44Leb
+Sevilla = m38EGiC3GF8kEXHZW
+Jaen = KnovkdwE8JQsigjSz
+Granada = Yzj9ouJvg3wmSGxjJ
+Malaga = kjcmzwptHwhaex245
+Huelva = N8GsB6dT972rT4tJx
+Almeria = kCNSEvXya9tERhe8M -->
+
+<!-- Cordoba = 08410
+Sevilla = 08390
+Jaen = 08417
+Granada = 08420
+Malaga = 08482
+Huelva = 08383
+Almeria = 08487
+Cadiz = 08452 -->
+
+
+
+
+
