8d19c0f1b3727bf0a5976492cc87aa7b0545439f
ruspat1 <ruspat1@us.es>
2023-05-19 17:33:22 +0200

    19-05

--- frontend/src/routes/integrations/environment-stats/intExternaWeather.svelte
+++ frontend/src/routes/integrations/environment-stats/intExternaWeather.svelte
@@ -14,41 +14,39 @@
   let apiData = "";
 
   const cityCoordinates = {
-  Cordoba: {
-    latitude: 37.888175,
-    longitude: -4.779383,
-  },
-  Sevilla: {
-    latitude: 37.389092,
-    longitude: -5.984459,
-  },
-  Jaen: {
-    latitude: 37.779594,
-    longitude: -3.784909,
-  },
-  Granada: {
-    latitude: 37.176487,
-    longitude: -3.597929,
-  },
-  Malaga: {
-    latitude: 36.721273,
-    longitude: -4.421399,
-  },
-  Huelva: {
-    latitude: 37.261421,
-    longitude: -6.944722,
-  },
-  Almeria: {
-    latitude: 36.83814,
-    longitude: -2.459743,
-  },
-  Cadiz: {
-    latitude: 36.527061,
-    longitude: -6.288596,
-  },
-};
-
-  
+    Cordoba: {
+      latitude: 37.888175,
+      longitude: -4.779383,
+    },
+    Sevilla: {
+      latitude: 37.389092,
+      longitude: -5.984459,
+    },
+    Jaen: {
+      latitude: 37.779594,
+      longitude: -3.784909,
+    },
+    Granada: {
+      latitude: 37.176487,
+      longitude: -3.597929,
+    },
+    Malaga: {
+      latitude: 36.721273,
+      longitude: -4.421399,
+    },
+    Huelva: {
+      latitude: 37.261421,
+      longitude: -6.944722,
+    },
+    Almeria: {
+      latitude: 36.83814,
+      longitude: -2.459743,
+    },
+    Cadiz: {
+      latitude: 36.527061,
+      longitude: -6.288596,
+    },
+  };
 
   let API_URL = "https://sos2223-10.ew.r.appspot.com/api/v3/environment-stats";
 
@@ -100,9 +98,76 @@
     chart.setOption(options);
   }
 
-  async function loadEchart2(apiData,airData) {
+  async function loadEchart2(apiData, airData) {
+    // Obtener el elemento HTML donde se mostrará la gráfica
+    const chartElement = document.getElementById("chart2");
+
+    // Crear la instancia de ECharts
+    const chart = echarts.init(chartElement);
+
+    // Configurar las opciones de la gráfica
+    const options = {
+      title: {
+        text: "Datos de ciudad, incendios, PM10 y PM25",
+      },
+      tooltip: {},
+      legend: {
+        data: ["Incendios", "PM10", "PM25"],
+      },
+      xAxis: {
+        data: apiData.map((data) => data.city),
+      },
+      yAxis: [
+        {
+          name: "Incendios",
+          type: "value",
+        },
+        {
+          name: "PM",
+          type: "value",
+        },
+      ],
+      series: [
+        {
+          name: "Incendios",
+          type: "scatter",
+          symbolSize: 10,
+          data: apiData.map((data) => [data.city, data.fire]),
+        },
+        {
+          name: "PM10",
+          type: "scatter",
+          symbolSize: 10,
+          yAxisIndex: 1,
+          data: apiData.map((data) => [
+            data.city,
+            airData[data.city]?.data[0]?.pm10,
+          ]),
+        },
+        {
+          name: "PM25",
+          type: "scatter",
+          symbolSize: 10,
+          yAxisIndex: 1,
+          data: apiData.map((data) => [
+            data.city,
+            airData[data.city]?.data[0]?.pm25,
+          ]),
+        },
+      ],
+    };
+
+    // Mostrar la gráfica
+    chart.setOption(options);
+  }
+  async function loadEcharts3(cricketData) {
+  // Filtrar los datos para obtener los valores de interés
+  const filteredData = cricketData.values.slice(0, 10); // Obtener hasta 10 jugadores
+  const names = filteredData.map((item) => item.values[1]);
+  const runs = filteredData.map((item) => parseInt(item.values[4]));
+
   // Obtener el elemento HTML donde se mostrará la gráfica
-  const chartElement = document.getElementById("chart2");
+  const chartElement = document.getElementById("chart3");
 
   // Crear la instancia de ECharts
   const chart = echarts.init(chartElement);
@@ -110,45 +175,22 @@
   // Configurar las opciones de la gráfica
   const options = {
     title: {
-      text: "Datos de ciudad, incendios, PM10 y PM25",
+      text: "Runs de jugadores de cricket",
     },
     tooltip: {},
-    legend: {
-      data: ["Incendios", "PM10", "PM25"],
-    },
     xAxis: {
-      data: apiData.map((data) => data.city),
+      type: "value",
+      name: "Runs",
     },
-    yAxis: [
-      {
-        name: "Incendios",
-        type: "value",
-      },
-      {
-        name: "PM",
-        type: "value",
-      },
-    ],
+    yAxis: {
+      data: names,
+      type: "category",
+    },
     series: [
       {
-        name: "Incendios",
-        type: "scatter",
-        symbolSize: 10,
-        data: apiData.map((data) => [data.city, data.fire]),
-      },
-      {
-        name: "PM10",
-        type: "scatter",
-        symbolSize: 10,
-        yAxisIndex: 1,
-        data: apiData.map((data) => [data.city, airData[data.city]?.data[0]?.pm10]),
-      },
-      {
-        name: "PM25",
-        type: "scatter",
-        symbolSize: 10,
-        yAxisIndex: 1,
-        data: apiData.map((data) => [data.city, airData[data.city]?.data[0]?.pm25]),
+        name: "Runs",
+        type: "bar",
+        data: runs,
       },
     ],
   };
@@ -157,11 +199,49 @@
   chart.setOption(options);
 }
 
+function loadEcharts4(coins) {
+  const chartData = coins.data.coins.map((coin) => ({
+    name: coin.name,
+    value: parseFloat(coin.price)
+  }));
+
+  const chart = echarts.init(document.getElementById('chart4'));
+
+  const option = {
+    tooltip: {
+      trigger: 'item',
+      formatter: '{b}: ${c}'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: chartData.map((coin) => coin.name)
+    },
+    series: [
+      {
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '60%'],
+        data: chartData,
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+  chart.setOption(option);
+}
 
 
-  
 
- 
+
+
+
 
   async function getData() {
     resultStatus = result = "";
@@ -202,50 +282,111 @@
     }
 
     const airData = {};
-for (const cityData of apiData) {
-  const city = cityData.city;
-  const coordinates = cityCoordinates[city];
-  
-  if (coordinates) {
-    const lat = coordinates.latitude;
-    const lon = coordinates.longitude;
-    
-    const options = {
-      method: 'GET',
-      url: 'https://air-quality.p.rapidapi.com/current/airquality',
-      params: {
-        lon: lon,
-        lat: lat
-      },
-      headers: {
-      //  'X-RapidAPI-Key': 'e52c3722d9msha0d5beb871b5cd4p1175cbjsnfae869968c4f',
-      'X-RapidAPI-Key':'dff350a5e9msh0576314cd591864p1be088jsn092c880c0a12',
-        'X-RapidAPI-Host': 'air-quality.p.rapidapi.com'
+    for (const cityData of apiData) {
+      const city = cityData.city;
+      const coordinates = cityCoordinates[city];
+
+      if (coordinates) {
+        const lat = coordinates.latitude;
+        const lon = coordinates.longitude;
+
+        const options = {
+          method: "GET",
+          url: "https://air-quality.p.rapidapi.com/current/airqualityyyyyyyyyyyy",
+          params: {
+            lon: lon,
+            lat: lat,
+          },
+          headers: {
+            //  'X-RapidAPI-Key': 'e52c3722d9msha0d5beb871b5cd4p1175cbjsnfae869968c4f',
+            "X-RapidAPI-Key":
+              "dff350a5e9msh0576314cd591864p1be088jsn092c880c0a12",
+            "X-RapidAPI-Host": "air-quality.p.rapidapi.com",
+          },
+        };
+
+        try {
+          const airResponse = await axios.request(options);
+          const airDataForCity = airResponse.data;
+          airData[cityData.city] = airDataForCity;
+          console.log(airData);
+        } catch (error) {
+          console.error(
+            `Error obteniendo la calidad del aire para ${city}: ${error}`
+          );
+        }
+      } else {
+        console.warn(`No se encontraron coordenadas para ${city}.`);
       }
-    };
-    
-    try {
-      const airResponse = await axios.request(options);
-        const airDataForCity = airResponse.data;
-        airData[cityData.city] = airDataForCity;
-        console.log(airData);
-    } catch (error) {
-      console.error(`Error obteniendo la calidad del aire para ${city}: ${error}`);
     }
-  } else {
-    console.warn(`No se encontraron coordenadas para ${city}.`);
+
+    console.log("a");
+    loadEchart(apiData, weatherData);
+    //   loadEchart2(apiData,airData);
   }
+
+let cricketData = {};
+async function getData2(){
+const options = {
+  method: 'GET',
+  url: 'https://cricbuzz-cricket.p.rapidapi.com/stats/v1/team/2',
+  params: {
+    statsType: 'mostRuns',
+    year: '2020'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'dff350a5e9msh0576314cd591864p1be088jsn092c880c0a12',
+    'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
+  }
+};
+
+
+try {
+	const response = await axios.request(options);
+  cricketData = response.data;
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
 }
 
+loadEcharts3(cricketData);
 
-    
-    console.log("a");
-        loadEchart(apiData, weatherData);
-     //   loadEchart2(apiData,airData);
+}
+let coins ={}; 
+async function getData3(){
+const options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/coins',
+  params: {
+    referenceCurrencyUuid: 'yhjMzLPhuIDl',
+    timePeriod: '24h',
+    'tiers[0]': '1',
+    orderBy: 'marketCap',
+    orderDirection: 'desc',
+    limit: '10',
+    offset: '0'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'dff350a5e9msh0576314cd591864p1be088jsn092c880c0a12',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
   }
+};
+
+try {
+	const response = await axios.request(options);
+   coins = response.data;
+    console.log(coins);
+} catch (error) {
+	console.error(error);
+}
+
+loadEcharts4(coins);
+}
 
   onMount(async () => {
     getData();
+    getData2();
+    getData3();
   });
 </script>
 
@@ -258,24 +399,14 @@ for (const cityData of apiData) {
 <main>
   <div id="chart" style="width: 1300px;height:400px;" />
   <div id="chart2" style="width: 1300px;height:400px;" />
+  <div id="chart3" style="width: 1300px;height:400px;" />
+  <div id="chart4" style="width: 800px; height: 600px;"></div>
+
 </main>
 
-<!-- Cordoba = 9ZMfhpfTHFfN44Leb
-Sevilla = m38EGiC3GF8kEXHZW
-Jaen = KnovkdwE8JQsigjSz
-Granada = Yzj9ouJvg3wmSGxjJ
-Malaga = kjcmzwptHwhaex245
-Huelva = N8GsB6dT972rT4tJx
-Almeria = kCNSEvXya9tERhe8M -->
 
-<!-- Cordoba = 08410
-Sevilla = 08390
-Jaen = 08417
-Granada = 08420
-Malaga = 08482
-Huelva = 08383
-Almeria = 08487
-Cadiz = 08452 -->
+
+
 
 
 
