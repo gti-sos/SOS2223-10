4370e069efc4669ee79bd6d45290c0623e4bd868
ruspat1 <ruspat1@us.es>
2023-05-20 14:45:46 +0200

    20-05

--- frontend/src/routes/graph/environment-stats/+page.svelte
+++ frontend/src/routes/graph/environment-stats/+page.svelte
@@ -13,15 +13,7 @@
     let resultStatus = "";
 
     async function loadEchart(graphData) {
-        graphData.sort((a, b) => {
-            if (b.year !== a.year) {
-                // ordena primero por year de mayor a menor
-                return a.year - b.year;
-            } else {
-                // si el year es igual, ordena por city en orden alfabético
-                return a.city.localeCompare(b.city);
-            }
-        });
+        
 
         console.log("graphData", graphData);
 
@@ -35,8 +27,14 @@
                 data: ["Incendios"],
             },
             xAxis: {
-                data: graphData.map((item) => `${item.year} - ${item.city}`),
+                data: graphData.map((item) => item.city),
+                axisLabel: {
+        formatter: function (value, index) {
+          const item = graphData[index];
+          return `${item.year} - ${item.city}`; // Mostrar año-ciudad en todas las barras
+        },
             },
+        },
             yAxis: {},
             series: [
                 {
@@ -49,160 +47,203 @@
 
         //Líneas
         var myChart2 = echarts.init(document.getElementById("container2"));
-        myChart2.setOption({
-            title: {
-                text: "Evolución de incendios por provincia y año",
-            },
-            tooltip: {
-                trigger: "axis",
-                axisPointer: {
-                    type: "cross",
-                    animation: false,
-                    label: {
-                        backgroundColor: "#ccc",
-                        borderColor: "#aaa",
-                        borderWidth: 1,
-                        shadowBlur: 0,
-                        shadowOffsetX: 0,
-                        shadowOffsetY: 0,
-                        textStyle: {
-                            color: "#000",
-                        },
-                    },
-                },
-            },
-            legend: {
-                data: graphData.map((item) => item.city),
-            },
-            xAxis: {
-                type: "category",
-                boundaryGap: false,
-                data: graphData
-                    .filter((item, index, self) => {
-                        // Filtrar duplicados en el eje x
-                        return (
-                            index ===
-                            self.findIndex(
-                                (t) =>
-                                    t.year === item.year &&
-                                    t.province === item.province
-                            )
-                        );
-                    })
-                    .map((item) => item.year),
-            },
-            yAxis: {
-                type: "value",
-                axisLabel: {
-                    formatter: "{value}",
-                },
-                min: 0,
-            },
-            series: graphData.map((item) => {
-                return {
-                    name: item.city,
-                    type: "line",
-                    data: graphData
-                        .filter(
-                            (t) =>
-                                t.city === item.city &&
-                                t.province === item.province &&
-                                t.year
-                        )
-                        .map((t) => t.fire),
-                    smooth: true,
-                };
-            }),
-        });
-        const myChart3 = echarts.init(document.getElementById("container3"));
+var cities = Array.from(new Set(graphData.map((item) => item.city))); // Obtener todas las ciudades únicas
+var seriesData = cities.map((city) => {
+  return {
+    name: city,
+    type: "line",
+    data: graphData
+      .filter((item) => item.city === city)
+      .map((item) => item.fire),
+  };
+});
 
-        // Define los colores para cada recurso
-        const colors = ["#5470c6", "#91cc75", "#fac858", "#ee6666", "#73c0de"];
+myChart2.setOption({
+  title: {
+    text: "Evolución de incendios por ciudad a lo largo de los años",
+    top: "1%",
+  },
+  grid: {
+    top: "20%", // Ajustar la distancia desde la parte superior
+    bottom: "10%", // Ajustar la distancia desde la parte inferior
+  },
+  tooltip: {
+    trigger: "axis",
+  },
+  legend: {
+    data: cities,
+    top:"10%",
+  },
+  xAxis: {
+    type: "category",
+    data: Array.from(new Set(graphData.map((item) => item.year))), // Obtener todos los años únicos
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: seriesData,
+});
 
-        myChart3.setOption({
-            title: {
-                text: "Número de incendios y área protegida por ciudad y año",
+const myChart3 = echarts.init(document.getElementById("container3"));
+
+// Obtén la lista de ciudades únicas
+
+// Define los colores para cada recurso
+const colors = ["#5470c6", "#91cc75", "#fac858"];
+
+
+
+
+myChart3.setOption({
+    title: {
+        text: "Evolución de incendios, área y espacios protegidos por ciudad y año",
+    },
+    tooltip: {},
+    legend: {
+        data: ["Incendios", "Área", "Espacios Protegidos"],
+        top: "10%",
+    },
+    grid: {
+    top: "20%", // Ajustar la distancia desde la parte superior
+    bottom: "10%", // Ajustar la distancia desde la parte inferior
+  },
+    xAxis: {
+        data: graphData.map((item) => `${item.city} - ${item.year}`),
+    },
+    yAxis: {
+        type: "value",
+    },
+    dataZoom: [
+        {
+            type: "inside",
+            start: 0,
+            end: 100,
+        },
+        {
+            type: "slider",
+            show: true,
+            y: "90%",
+            start: 0,
+            end: 100,
+        },
+    ],
+    series: [
+        {
+            name: "Incendios",
+            type: "bar",
+            itemStyle: {
+                color: colors[0],
             },
-            tooltip: {},
-            legend: {
-                data: ["Número de incendios", "Área protegida"],
+            data: graphData.map((item) => item.fire),
+        },
+        {
+            name: "Área",
+            type: "bar",
+            itemStyle: {
+                color: colors[1],
             },
-            xAxis: {
-                data: graphData.map((item) => `${item.year} - ${item.city}`),
+            data: graphData.map((item) => item.area),
+        },
+        {
+            name: "Espacios Protegidos",
+            type: "bar",
+            itemStyle: {
+                color: colors[2],
             },
-            yAxis: {},
-            series: [
-                {
-                    name: "Número de incendios",
-                    type: "bar",
-                    stack: "total",
-                    itemStyle: {
-                        color: colors[0],
-                    },
-                    data: graphData.map((item) => item.fire),
+            data: graphData.map((item) => item.protected_space),
+        },
+    ],
+    color: colors,
+});
+
+// Agrega la funcionalidad de selección de eje y
+document.getElementById("selectYAxis").addEventListener("change", (event) => {
+    const selectedYAxis = event.target.value;
+    const seriesIndex = ["Incendios", "Área", "Espacios Protegidos"].indexOf(selectedYAxis);
+    myChart3.setOption({
+        yAxis: {
+            type: "value",
+        },
+        series: [
+            {
+                name: selectedYAxis,
+                type: "bar",
+                itemStyle: {
+                    color: colors[seriesIndex],
                 },
-                {
-                    name: "Área protegida",
-                    type: "bar",
-                    stack: "total",
-                    itemStyle: {
-                        color: colors[1],
-                    },
-                    data: graphData.map((item) => item.area),
-                },
-            ],
-            color: colors,
-        });
+                data: graphData.map((item) => item[selectedYAxis.toLowerCase().replace(" ", "_")]),
+            },
+        ],
+    });
+});
+
+
+
+
+
+
     }
+
+
+
+      
+
+
 
     async function loadHighchart(graphData) {
-        const chartData = graphData.reduce((acc, data) => {
-            const { city, protected_space, area } = data;
-            acc[city] = acc[city] || { protected_space: 0, area: 0 };
-            acc[city].protected_space += protected_space;
-            acc[city].area += area;
-            return acc;
-        }, {});
+  const chartData = graphData.reduce((acc, data) => {
+    const { city, protected_space, area } = data;
+    acc[city] = acc[city] || { protected_space: 0, area: 0 };
+    acc[city].protected_space += protected_space;
+    acc[city].area += area;
+    return acc;
+  }, {});
 
-        const seriesData = Object.entries(chartData).map(
-            ([city, { protected_space, area }]) => ({
-                name: city,
-                y: protected_space,
-                extra: area,
-            })
-        );
+  const seriesData = Object.entries(chartData).map(([city, { protected_space, area }]) => ({
+    name: city,
+    y: protected_space *30 / area * 100, // Calcular el porcentaje
+    extra: area,
+  }));
 
-        Highcharts.chart("containerPie", {
-            chart: {
-                type: "bar",
-            },
-            title: {
-                text: "Comparación de áreas protegidas y áreas en la ciudad por ciudad",
-            },
-            tooltip: {
-                pointFormat:
-                    '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y}</b> ({point.percentage:.1f}%)<br/>' +
-                    "Área total: <b>{point.extra}</b>",
-            },
-            plotOptions: {
-                bar: {
-                    allowPointSelect: true,
-                    cursor: "pointer",
-                    dataLabels: {
-                        enabled: true,
-                        format: "<b>{point.name}</b>: {point.y}",
-                    },
-                    showInLegend: true,
-                },
-            },
-            series: [
-                {
-                    name: "Áreas protegidas",
-                    data: seriesData,
-                },
-            ],
-        });
-    }
+  Highcharts.chart("containerPie", {
+    chart: {
+      type: "area", // Cambiar a tipo "area"
+    },
+    title: {
+      text: "Comparación de áreas protegidas y áreas en la ciudad por ciudad",
+    },
+    xAxis: {
+      categories: seriesData.map((data) => data.name), // Configurar las categorías en el eje x
+    },
+    yAxis: {
+      title: {
+        text: "Porcentaje", // Configurar el título del eje y
+      },
+    },
+    tooltip: {
+      pointFormat:
+        '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.2f}%</b><br/>' + // Mostrar el porcentaje con dos decimales
+        "Área total: <b>{point.extra}</b>",
+    },
+    plotOptions: {
+      area: {
+        dataLabels: {
+          enabled: true,
+          format: "{point.y:.2f}%", // Mostrar el porcentaje en el gráfico
+        },
+      },
+    },
+    series: [
+      {
+        name: "Áreas protegidas",
+        data: seriesData,
+      },
+    ],
+  });
+}
+
+
+
 
     async function getData() {
         resultStatus = result = "";
@@ -227,7 +268,10 @@
     onMount(async () => {
         getData();
     });
+
+
 </script>
+
 
 <svelte:head>
     <script
@@ -239,9 +283,9 @@
 <main>
     <div id="containerPie" style="width: 1300px;height:400px;" />
 
-    <div id="container" style="width: 1300px;height:400px;" />
+    <div id="container" style="width: 1400px;height:400px;" />
 
-    <body>Hola</body>
+ 
     <div id="chart" />
 
     <div id="container2" style="width: 1300px;height:400px;" />
